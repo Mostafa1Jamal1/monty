6ae0353479b384e1222d1d11d6ae6cb14691606c
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-09-21 02:40:54 +0300

    the main file

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * main - Monty interpreter
+ * @argc: number of arguments (should be 2)
+ * @argv: an array of string arguments
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+
+FILE *Gfile;
+
+int main(int argc, char **argv)
+{
+	unsigned int line_number = 1;
+	stack_t *stack = NULL;
+	/* Handle usage failure */
+	if (argc != 2)
+		exit_usagefail();
+	/* Handle open file */
+	Gfile = fopen(argv[1], "r");
+	if (!Gfile)
+		exit_openfail(argv[1]);
+	/* Handle interpreting lines */
+	while (interpret_line(Gfile, &stack, line_number))
+		line_number++;
+	/* Finished file */
+	fclose(Gfile);
+	free_stack(stack);
+	return (EXIT_SUCCESS);
+}
