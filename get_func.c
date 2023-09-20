ada14c2d84fdc5cc0c66f3bc94632c201947c257
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-09-21 02:44:37 +0300

    get function of opcode

---
+++ get_func.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * get_func - get the function of the opcode
+ * @opcode: the opcode
+ * Return: function pointer of the opcode
+ * or NULL if not found
+ */
+void (*get_func(char *opcode))(stack_t **stack, unsigned int line_number)
+{
+	int i;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+
+	for (i = 0; (instructions + i)->opcode != NULL; i++)
+	{
+		if (strcmp(opcode, (instructions + i)->opcode) == 0)
+			return ((instructions + i)->f);
+	}
+	return (NULL);
+}
