051d55a2831d62894426f9f27be477cae27541cc
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-09-21 15:12:39 +0300

    Implement the div opcode.

---
+++ div.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * div - divides the second top element of the stack
+ * by the top element of the stack.
+ * @stack: a pointer to stack
+ * @line_number: the line number
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top = *stack, *next;
+
+	if (top)
+	{
+		next = top->next;
+		if (next)
+		{
+			if (top->n == 0)
+			{
+				fprintf(stderr, "L%u: division by zero\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+			next->n /= top->n;
+			free(*stack);
+			*stack = next;
+			next->prev = NULL;
+			return;
+		}
+	}
+	fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+	exit(EXIT_FAILURE);
+}
--- get_func.c
+++ get_func.c
@@ -17,6 +17,7 @@ void (*get_func(char *opcode))(stack_t **stack, unsigned int line_number)
 		{"add", add},
 		{"nop", nop},
 		{"sub", sub},
+		{"div", div},
 		{NULL, NULL}
 	};
 
